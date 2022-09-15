6b5d9cf2fac9cc7050eb447aa7dd2b77f4e23c39
BakedRice14 <ahmadfauzirizky003@gmail.com>
2022-09-16 06:57:48 +0700

    claening my repo

--- _CodeChefPractice.cpp
+++ _CodeChefPractice.cpp
@@ -3,49 +3,22 @@
 using namespace std;
 int main()
 {
-    int min;
     int t;
-    int n = INT_MAX;
-    string v;
-    // 5
-    // 3
-    // 5 10 3
-    // 000
-    // 3
-    // 5 10 3
-    // 001
-    // 3
-    // 5 5 3
-    // 001
-    // 3
-    // 5 5 3
-    // 101
-    // 5
-    // 10 100 100 10 10
-    // 00001
     cin >> t;
     for (int i = 0; i < t; i++)
     {
-        min = INT_MAX;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++)
+        int ans, k, x;
+        cin >> k >> x;
+        ans = k - x;
+        if (k < x)
         {
-            cin >> a[i];
+            break;
         }
-        cin >> v;
-        for (int i = 0; i < v.length(); i++)
+        else
         {
-            if (v[i] == '0')
-            {
-                if (a[i] < min)
-                {
-                    min = a[i];
-                }
-            }
+            cout << ans << endl;
         }
     }
 
-    cout << min;
     return 0;
 }
