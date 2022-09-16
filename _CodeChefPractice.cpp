443904d251a9cba07309587d492ea1bca9f823fe
BakedRice14 <ahmadfauzirizky003@gmail.com>
2022-09-16 21:43:35 +0700

    hello wold

--- _CodeChefPractice.cpp
+++ _CodeChefPractice.cpp
@@ -3,20 +3,6 @@
 using namespace std;
 int main()
 {
-    int t;
-    cin >> t;
-    for (int i = 0; i < t; i++)
-    {
-        int mod, x, y, z;
-        double ans;
-        cin >> x >> y >> z;
-        x *= 5;
-        y *= 10;
-        mod = (x + y) % z;
-        ans = double(x + y) / z;
-        ans = floor(ans);
-        cout << ans << endl;
-    }
-
+    cout << "hello world";
     return 0;
 }
