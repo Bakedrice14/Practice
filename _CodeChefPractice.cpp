e7fc91be9225aaa51a7754b12f076de99ce9cd2b
BakedRice14 <ahmadfauzirizky003@gmail.com>
2022-09-18 21:07:23 +0700

    solved _CodeChefPractice.cpp 'Red Light, Green Light'

--- _CodeChefPractice.cpp
+++ _CodeChefPractice.cpp
@@ -6,13 +6,18 @@ int main(int argc, char const *argv[])
     cin >> t;
     while (t--)
     {
-        int n, m, ans;
-        cin >> n >> m;
-        n *= 2;
-        m *= 4;
-        ans = n + m;
-        cout << ans << endl;
+        int x, y, inc = 0;
+        cin >> x >> y;
+        for (int i = 0; i < x; i++)
+        {
+            int h[x];
+            cin >> h[i];
+            if (y < h[i])
+            {
+                inc++;
+            }
+        }
+        cout << inc << endl;
     }
-
     return 0;
 }
